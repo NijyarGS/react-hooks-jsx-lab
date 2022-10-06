e9d0502d0cfe8cfa5e974f205a1b21e39a609e2f
Nijyar Ghazi <nijyar360@gmail.com>
2022-10-06 23:13:13 +0300

    Completed assigment

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,6 @@
 // use these variables in your JSX (update them with your own name and city!)
-const name = "Liza";
-const city = "New York";
+const name = "nijyar";
+const city = "Duhok";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
